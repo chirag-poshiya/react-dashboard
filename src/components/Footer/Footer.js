360491ca80e3e0a2e349a04ad37752245b644a8d
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-07-27 16:35:25 +0530

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,30 +3,33 @@
 import React from "react";
 // reactstrap components
 
-import { Container, Nav, NavItem, NavLink } from "reactstrap";
+import { 
+  Container, 
+  Nav, 
+  NavItem, 
+  NavLink 
+} from "reactstrap";
+
 // footer function
 function Footer() {
   return (
-    <footer className="footer">
+    <footer 
+      className="footer">
       <Container fluid>
         <Nav>
           <NavItem>
-            <NavLink href="https://www.desilo.studio/about">
+            <NavLink 
+              href="https://www.desilo.studio/about">
               About Us
             </NavLink>
-          </NavItem>
-         
+          </NavItem> 
         </Nav>
         <div className="copyright">
           © {new Date().getFullYear()} made with{" "}
           <i className="ch-icons icon-heart-2" /> by{" "}
-          <a
-            href="https://www.desilo.studio"
-            target="_blank"
-          >
+          <a href="https://www.desilo.studio" target="_blank" >
             Desilo
-          </a>{" "}
-          for a better web.
+          </a>{" "} for a better web.
         </div>
       </Container>
     </footer>
--- src/components/ThemeWrapper/ThemeWrapper.js
+++ src/components/ThemeWrapper/ThemeWrapper.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { ThemeContext, themes } from "contexts/ThemeContext";
+import { 
+  ThemeContext, 
+  themes 
+} from "contexts/ThemeContext";
 
 export default function ThemeContextWrapper(props) {
+  
   const [theme, setTheme] = useState(themes.dark);
 
   function changeTheme(theme) {
